a23ef9a35cdd6834c0af71a65dc77caefa084884
Steven Buller <steven@Stevens-MacBook-Pro.local>
2016-01-07 18:31:03 -0500

    the image counter is working

--- home.js
+++ home.js
@@ -4,41 +4,51 @@ $(document).ready(function(){
   var secondsCounter = 0;
   var timerInterval;
   var checkTimer;
+  var imageCounter = 0;
 
   $("#startStopBtn").on("click", function(e) {
     e.preventDefault();
     if (this.getAttribute("data-state") === "start") {
-      alert("this works");
+      //alert("this works");
       $("#startStopBtn").attr("data-state", "stop");
       $("#startStopBtn").html("Click To Stop");
-      //image counter = 0
+      imageCounter = 0;
       secondsCounter = 0;
-      //a timer is started and checked every second
+      //a timer is started and secondsCounter is incremented every second
       timerInterval = setInterval(incrementTimer, 1000);
+      //the secondsCounter is checked every second
       checkTimer = setInterval(checkSecondsCounter, 1000);
+      //if a button is clicked increment the image counter
+      $(".btn-lg").on("click", function(e) {
+        e.preventDefault();
+        imageCounter++;
+        alert("the counter is " + imageCounter);
+      });
     }
     else if (this.getAttribute("data-state") === "stop"){
-      alert("the data-state is stop");
+      //removes the event handler from the buttons
+      $(".btn-lg").off("click");
       $("#startStopBtn").attr("data-state", "start");
       $("#startStopBtn").html("Click To Start");
-      //timer is stopped  / reset
-      //image counter is set to zero
-
+      //the timer is stopped and secondsCounter set to zero
+      clearInterval(timerInterval); 
+      clearInterval(checkTimer);
+      secondsCounter = 0;
     }
 
 
     function incrementTimer() { 
       secondsCounter++;
-      alert("The timer is " + secondsCounter);
      }
 
-     //checks seconds counter, if >= 20 seconds it ends the game
+    //checks secondsCounter, if >= 20 seconds it ends the game
     function checkSecondsCounter() {
       if (secondsCounter >= 20 ) {
         $("#startStopBtn").attr("data-state", "start");
         $("#startStopBtn").html("Click To Start");
         clearInterval(timerInterval); 
         clearInterval(checkTimer);
+        $(".btn-lg").off("click");
         alert("Time is up " + secondsCounter + " seconds");
       } 
     }
